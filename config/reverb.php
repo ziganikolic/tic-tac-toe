0efb023e5f69b979a9d9f86a85138e378b537b48
Žiga Nikolić <93379542+ziganikolic@users.noreply.github.com>
2025-08-11 09:47:47 +0200

    Add basic Reverb configuration

---
+++ config/reverb.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'apps' => [
+        [
+            'id' => env('REVERB_APP_ID'),
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
+        ],
+    ],
+
+    'host' => env('REVERB_HOST', '127.0.0.1'),
+    'port' => env('REVERB_PORT', 8080),
+    'scheme' => env('REVERB_SCHEME', 'http'),
+];
