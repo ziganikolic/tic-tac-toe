16cc027eec6348c7e8627d064dd15d7c640d2b12
Žiga Nikolić <93379542+ziganikolic@users.noreply.github.com>
2025-08-11 11:13:27 +0200

    Fix Pusher configuration for Reverb

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    envPrefix: ['VITE_', 'REVERB_'],
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
