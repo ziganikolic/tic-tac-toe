c9532c824b6e64a09922bd83951136ef28f67ad5
Žiga Nikolić <93379542+ziganikolic@users.noreply.github.com>
2025-09-03 14:51:30 +0200

    Use Pusher for realtime broadcasting

--- resources/js/Realtime/echo.ts
+++ resources/js/Realtime/echo.ts
@@ -7,12 +7,20 @@ import Pusher from 'pusher-js'
 console.log('Echo config (Pusher):', {
   key: import.meta.env.VITE_PUSHER_APP_KEY,
   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-  forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https',
+  host: import.meta.env.VITE_PUSHER_HOST,
+  port: import.meta.env.VITE_PUSHER_PORT,
+  scheme: import.meta.env.VITE_PUSHER_SCHEME,
 })
 
 export const echo = new Echo({
   broadcaster: 'pusher',
   key: import.meta.env.VITE_PUSHER_APP_KEY,
-  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-  forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https',
+  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+  wsHost:
+    import.meta.env.VITE_PUSHER_HOST ??
+    `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+  wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+  wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+  forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
 })
