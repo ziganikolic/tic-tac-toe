f0ef593687dce25454bd808286d653e40726b875
ziganikolic <ziganikolic@gmail.com>
2025-09-25 21:05:22 +0200

    fd

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -1,12 +1,18 @@
 <?php
 
 return [
-    'default' => env('BROADCAST_CONNECTION', 'log'),
+    'default' => env('BROADCAST_CONNECTION', env('ABLY_KEY') ? 'ably' : 'log'),
 
     'connections' => [
         'ably' => [
             'driver' => 'ably',
-            'key' => env('ABLY_KEY'),
+            'key' => env('ABLY_KEY', env('PUSHER_APP_KEY')),
+        ],
+
+        // Backwards compatible alias so old BROADCAST_CONNECTION=pusher configs still work.
+        'pusher' => [
+            'driver' => 'ably',
+            'key' => env('ABLY_KEY', env('PUSHER_APP_KEY')),
         ],
 
         'log' => [
